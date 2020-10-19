9f464841a1e5f35ec84577862827e1935551b774
LakshmiGanesh <71899781+LakshmiGanesh99@users.noreply.github.com>
2020-10-20 00:33:02 +0530

    [LakshmiGanesh] add: ability to search for node

--- BinarySearchTree/src/test/java/com/capgemini/binarySearchTree/MyBinaryTreeTest.java
+++ BinarySearchTree/src/test/java/com/capgemini/binarySearchTree/MyBinaryTreeTest.java
@@ -1,6 +1,5 @@
 package com.capgemini.binarySearchTree;
 
-
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -35,5 +34,24 @@ public class MyBinaryTreeTest {
 		int size = myBinaryTree.getSize();
 		Assert.assertEquals(13, size);
 		
+	}
+	
+	@Test
+	public void given13Numbers_When63Found_ShouldPassTheTest() {
+		MyBinaryTree<Integer> myBinaryTree = new MyBinaryTree<>();
+		myBinaryTree.add(56);
+		myBinaryTree.add(30);
+		myBinaryTree.add(70);
+		myBinaryTree.add(22);
+		myBinaryTree.add(40);
+		myBinaryTree.add(11);
+		myBinaryTree.add(3);
+		myBinaryTree.add(16);
+		myBinaryTree.add(60);
+		myBinaryTree.add(65);
+		myBinaryTree.add(63);
+		myBinaryTree.add(67);
+		myBinaryTree.add(95);
+		Assert.assertTrue(myBinaryTree.search(63));
 	}
 }
